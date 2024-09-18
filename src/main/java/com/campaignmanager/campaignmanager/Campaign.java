32f07f7a468f12529957a8964ac0df9420183d71
matejj7 <mateusz2003matyja@gmail.com>
2024-09-18 13:39:11 +0200

    Added relation in the DB

--- src/main/java/com/campaignmanager/campaignmanager/Campaign.java
+++ src/main/java/com/campaignmanager/campaignmanager/Campaign.java
@@ -1,9 +1,6 @@
 package com.campaignmanager.campaignmanager;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
@@ -30,6 +27,10 @@ public class Campaign {
     private Double radius;
     @NotNull
     private Double campaignFund;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
 
 
 }
